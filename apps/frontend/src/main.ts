c9e986fa9eb58ec936f92203b3cf95a53842c53e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-11-05 21:34:03 +0900

    chore: apply i18n

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -2,7 +2,21 @@ import '/@/assets/styles/main.scss'
 import '/@/assets/styles/tailwind.scss'
 
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 
+import en from '/@/locales/en.json'
+import ja from '/@/locales/ja.json'
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  inheritLocale: true,
+  messages: {
+    en,
+    ja,
+  },
+})
 import App from '/@/App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(i18n).mount('#app')
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,23 @@
+import i18n from '@intlify/vite-plugin-vue-i18n'
 import { join } from 'path'
+import analyze from 'rollup-plugin-analyzer'
+import visualizer from 'rollup-plugin-visualizer'
 import type { UserConfig } from 'vite'
+import vitePluginYaml from 'vite-plugin-yaml'
+
 const config: UserConfig = {
   alias: {
     '/@/': join(__dirname, 'src'),
+  },
+
+  vueCustomBlockTransforms: {
+    i18n,
+  },
+
+  plugins: [vitePluginYaml],
+
+  rollupInputOptions: {
+    plugins: [analyze({ summaryOnly: true }), visualizer()],
   },
 }
 
