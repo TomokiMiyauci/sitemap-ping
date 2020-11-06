4e0e86a43249aba5a946fe382adaf20221d8b791
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-11-06 23:33:38 +0900

    feat: add new components

---
+++ apps/frontend/src/components/app/LinkRepo.vue
@@ -0,0 +1,20 @@
+<template>
+  <a href="https://github.com/TomokiMiyauci/sitemaping" target="_brank">
+    <mdi-github class="w-12 h-12" />
+  </a>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  import MdiGithub from '/@/components/base/icons/MdiGithub.vue'
+  export default defineComponent({
+    components: {
+      MdiGithub,
+    },
+
+    setup() {
+      console.log(process.env)
+    },
+  })
+</script>
